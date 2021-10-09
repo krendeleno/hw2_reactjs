211a1b3884c54010ec86f2c8edd7eb6068577f13
krendeleno <krendeleno@gmail.com>
2021-10-09 15:54:50 +0300

    packages fix and icons

---
+++ src/components/icons/12Play.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+function Svg12Play(props) {
+  return (
+    <svg
+      viewBox="0 0 9 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M8.289 4.193L1.414.128C.855-.202 0 .118 0 .935v8.127a.937.937 0 001.414.806l6.875-4.062a.936.936 0 000-1.613z"
+        fill="#111"
+      />
+    </svg>
+  );
+}
+
+export default Svg12Play;
